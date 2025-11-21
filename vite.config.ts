a7f8b19cef7f2db320b451de3b9531d039b1be22
loaigamal-ai <loaigamal20003@gmail.com>
2025-11-21 07:23:55 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from 'Nono-Gymnastics-Ai-assistant';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
